b67777f0d0f0b27f199436d1fec5915d28aec4a4
Rok Sprogar <rok.sprogar@gmail.com>
2022-08-03 10:39:12 +0200

    start command and first route

---
+++ scaffold/server/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = 3001;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
