1623ea165f56f610a02e62c62f13519cfbd92f80
Rok Sprogar <rok.sprogar@gmail.com>
2022-08-03 20:05:49 +0200

    cleaned up the react app and enabled hot reloading

--- scaffold/client/src/App.js
+++ scaffold/client/src/App.js
@@ -1,56 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const API_HOST = process.env.REACT_APP_API_HOST || 'localhost';
-  const API_PORT = process.env.REACT_APP_API_PORT || 3002;
-  const API_BASE_ADDRESS = `http://${API_HOST}:${API_PORT}`;
+  // const API_HOST = process.env.REACT_APP_API_HOST || 'localhost';
+  // const API_PORT = process.env.REACT_APP_API_PORT || 3002;
+  // const API_BASE_ADDRESS = `http://${API_HOST}:${API_PORT}`;
 
-  const [response, setResponse] = useState('');
-  const [mongoHealth, setMongoHealth] = useState('fail');
+  // const [response, setResponse] = useState('');
+  // const [mongoHealth, setMongoHealth] = useState('fail');
 
-  useEffect(() => {
-    // console.log('Request on address: ', API_BASE_ADDRESS);
-    const fetchData = async () => {
-      const result = await axios.get(`${API_BASE_ADDRESS}/api/`);
-      setResponse(result.data);
-    };
-    fetchData();
-  }, [API_BASE_ADDRESS]);
+  // useEffect(() => {
+  //   // console.log('Request on address: ', API_BASE_ADDRESS);
+  //   const fetchData = async () => {
+  //     const result = await axios.get(`${API_BASE_ADDRESS}/api/`);
+  //     setResponse(result.data);
+  //   };
+  //   fetchData();
+  // }, [API_BASE_ADDRESS]);
 
-  useEffect(() => {
-    const checkHeath = async () => {
-      const result = await axios.get(`${API_BASE_ADDRESS}/api/healthcheck`);
-      setMongoHealth(result.data.status);
-    };
-    checkHeath();
-    const checkStatusTimer = setTimeout(() => checkHeath(), 10000);
-    return () => {
-      clearInterval(checkStatusTimer);
-    };
-  }, [API_BASE_ADDRESS]);
+  // useEffect(() => {
+  //   const checkHeath = async () => {
+  //     const result = await axios.get(`${API_BASE_ADDRESS}/api/healthcheck`);
+  //     setMongoHealth(result.data.status);
+  //   };
+  //   checkHeath();
+  //   const checkStatusTimer = setTimeout(() => checkHeath(), 10000);
+  //   return () => {
+  //     clearInterval(checkStatusTimer);
+  //   };
+  // }, [API_BASE_ADDRESS]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          The response is: {response}
-        </a>
-        <h4>The status of the MongoDb connection is: {mongoHealth}</h4>
-      </header>
-    </div>
-  );
+  return <div className="App">Welcome to the react app on docker!</div>;
 }
 
 export default App;
