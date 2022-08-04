38e55185b0b6f57863dbc32b9672f03665310d39
Rok Sprogar <rok.sprogar@gmail.com>
2022-08-04 22:09:34 +0200

    added note list api call

--- scaffold/client/src/components/Notes.js
+++ scaffold/client/src/components/Notes.js
@@ -1,41 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Note from './Note';
+import axios from 'axios';
 
 function Notes() {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    async function fetchNotes() {
+      const { data } = await axios.get('http://localhost/api/notes');
+      console.log('data', data.data);
+      setNotes(data.data);
+    }
+    fetchNotes();
+  }, []);
+
   return (
     <section>
       <div className="container">
         <div className="row">
-          <Note
-            title="Note 1"
-            description="1 Lorem ipsum dolot amer sit! Lorem ipsum dolot amer sit! Lorem
-              ipsum dolot amer sit! Lorem ipsum dolot amer sit!"
-          />
-          <Note
-            title="Note 2"
-            description="2 Lorem ipsum dolot amer sit! Lorem ipsum dolot amer sit! Lorem
-              ipsum dolot amer sit! Lorem ipsum dolot amer sit!"
-          />
-          <Note
-            title="Note 3"
-            description="3 Lorem ipsum dolot amer sit! Lorem ipsum dolot amer sit! Lorem
-              ipsum dolot amer sit! Lorem ipsum dolot amer sit!"
-          />
-          <Note
-            title="Note 4"
-            description="4 Lorem ipsum dolot amer sit! Lorem ipsum dolot amer sit! Lorem
-              ipsum dolot amer sit! Lorem ipsum dolot amer sit!"
-          />
-          <Note
-            title="Note 5"
-            description="5 Lorem ipsum dolot amer sit! Lorem ipsum dolot amer sit! Lorem
-              ipsum dolot amer sit! Lorem ipsum dolot amer sit!"
-          />
-          <Note
-            title="Note 6"
-            description="6 Lorem ipsum dolot amer sit! Lorem ipsum dolot amer sit! Lorem
-              ipsum dolot amer sit! Lorem ipsum dolot amer sit!"
-          />
+          {notes &&
+            notes.map((note) => (
+              <Note
+                key={note._id}
+                title={note.title}
+                description={note.description}
+              />
+            ))}
         </div>
       </div>
     </section>
